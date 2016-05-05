e19d2b6aecddd819d50ad86cc30638ee5f7664c1
Charles "Demurgos" Samborski <demurgos.net@gmail.com>
2016-05-05 19:15:51 +0200

    Root augmentation algorithm

--- doc/requirejs-decompiler.md
+++ doc/requirejs-decompiler.md
@@ -1,5 +1,23 @@
 # Require.js Decompiler
 
+## Algorithm
+
+### Naive
+
+This just searches for `define` calls with their first argument being an id string.
+
+The problem with this algorithm is that it does not take care of the scope.
+
+### Territory flags
+
+We first discover every `define` call and assign an id to each of them.
+We then proceed to explore
+
+Then, we mark each sub-node (and the call himself) by an id pointing to the nearest ancestor `define` (called the `owner`).
+At this point, some nodes don't have any owner (their `owner` attribute is `null`) and some owned nodes might referer to external nodes.
+
+Once every node is marked we traverse the tree to the root and note how many
+
 ## API
 
 ### `requireJS.decompile(options: DecompilerOptions): Promise<void>`
--- requirejs-decompiler/decompile.js
+++ requirejs-decompiler/decompile.js
@@ -4,6 +4,7 @@ var Bluebird = require("bluebird");
 var esprima = require("esprima");
 var escodegen = require("escodegen");
 var esquery = require("esquery");
+var estraverse = require("estraverse");
 var _ = require("lodash");
 
 var defaultOptions = {
@@ -26,38 +27,123 @@ var escodegenOptions = {
   }
 };
 
-/**
- * Searches the programNode to find the requireJS modules.
- *
- * @param programNode {Node} The programNode node
- * @return {Expression[]} The list of modules in the order of appearance in the astNode
- */
-function getModuleNodes (programNode) {
-  // Immediately-invoked function expression
-  var results = esquery.query(programNode, "Program > ExpressionStatement > CallExpression > FunctionExpression");
-  if (results.length !== 1) {
-    throw new Error("Expected exactly one IIFE at the root");
-  }
-  var iife = results[0];
-  var iifeBody = iife.body;
+var decompilerPrefix = "__";
+var uid = 0;
 
-  if (iifeBody.type !== "BlockStatement") {
-    throw new Error("Expected IIFE body to be a block statement");
-  }
-
-  var iifeNodes = iifeBody.body;
-
-  var defineCalls = esquery.query(iifeBody, "CallExpression[callee.type=Identifier][callee.name=define]");
-
-  return _.map(defineCalls, describeDefineCall);
+function getUid () {
+  return decompilerPrefix + uid++;
 }
 
-function describeDefineCall(defineCallNode) {
-  var args = defineCallNode.arguments;
-  if (args.length > 1 && args[0].type === "Literal" && typeof args[0].value === "string") {
-    return {id: args[0].value, ast: defineCallNode};
+function isDefineZoneRoot(astNode) {
+  return astNode._ownerDefine && astNode._ownerDefine._defineRoot === astNode;
+}
+
+/**
+ * Init the AST
+ * @param programNode
+ */
+function initTree (programNode) {
+  estraverse.traverse(programNode, {
+    enter: function(node, parent) {
+      node._parent = parent || null;
+      node._ownerDefine = null;
+      node._subDefines = 0;
+      node._isDefine = false;
+    }
+  });
+  return programNode;
+}
+
+/**
+ * Find all the define calls and mark them.
+ * @param node
+ * @returns {*}
+ */
+function markDefineCalls (programNode) {
+  var defineCalls = esquery.query(programNode, "CallExpression[callee.type=Identifier][callee.name=define]");
+
+  defineCalls = defineCalls.filter(function(dc) {
+    return dc.arguments.length > 1 && dc.arguments[0].type === "Literal" && typeof dc.arguments[0].value === "string";
+  });
+
+  _.forEach(defineCalls, function(dc) {
+    dc._ownerDefine = dc;
+    dc._defineRoot = dc;
+
+    dc._isDefine = true;
+    dc._isNestedDefine = false;
+    dc._nestedOwner = null; // If this is a nestedDefine, nestedOwner points to the parent defineCall
+  });
+  return defineCalls;
+}
+
+function markDefineCallsDescendants (programNode) {
+  estraverse.traverse(programNode, {
+    enter: function(node, parent) {
+      var curOwner = parent ? parent._ownerDefine : null;
+      if (!node._isDefine) {
+        node._ownerDefine = curOwner;
+      } else {
+        if (curOwner !== null) {
+          node._isNestedDefine = true;
+          node._nestedOwner = curOwner;
+        }
+      }
+    },
+    leave: function(node, parent) {
+      if (parent) {
+        if (node._isDefine) {
+          parent._subDefines++; // Nested define are merged into the top define for the subDefines count
+        } else {
+          parent._subDefines += node._subDefines;
+        }
+      }
+    }
+  });
+  return programNode;
+}
+
+/**
+ * Moves the root of the define subtree while there is no conflict.
+ * @param defineRootNode
+ * @return {boolean} Indicate if the root changed or not
+ */
+function augmentRootNoConflict(defineCall) {
+  if (defineCall._isNestedDefine) {
+    return false;
   }
-  return {id: null, ast: defineCallNode};
+
+  var root = defineCall._defineRoot;
+  if (!root._parent || root._parent._subDefines !== 1) {
+    return false;
+  }
+
+  do {
+    root = root._parent;
+    defineCall._defineRoot = root;
+    root._ownerDefine = defineCall;
+  } while(root._parent && root._parent._subDefines === 1);
+
+  return true;
+}
+
+/**
+ * maps to {id: string, filePath: string, ast: node}
+ */
+function getModuleDescriptors (defineCalls, options) {
+  return _.map(defineCalls, function(dc, idx) {
+    var id = dc.arguments[0].value;
+    var filePath = path.resolve(options.outputDir, id);
+
+    if (true) { // TODO: check if there is a need to add the extension ?
+      filePath = filePath + "." + options.fileExtension;
+    }
+    if (filePath.indexOf(options.outputDir) !== 0) {
+      throw new Error("filePath MUST be in outputDir");
+    }
+
+    return {id: id, filePath: filePath, ast: dc._defineRoot};
+  });
 }
 
 /**
@@ -93,17 +179,6 @@ function writeAstNode(filePath, astNode) {
   });
 }
 
-function getModulePath(id, options) {
-  var modulePath = path.resolve(options.outputDir, id);
-  if (true) { // TODO: check if there is a need to add the extension ?
-    modulePath = modulePath + "." + options.fileExtension;
-  }
-  if (modulePath.indexOf(options.outputDir) !== 0) {
-    throw new Error("ModulePath MUST be in outputDir");
-  }
-  return modulePath;
-}
-
 /**
  *
  * @param moduleDescriptors {{id: string, ast: Node}}
@@ -114,17 +189,9 @@ function writeModules(moduleDescriptors, options) {
       return fs.emptyDir(options.outputDir, cb);
     })
     .then(function () {
-      var promises = _.map(moduleDescriptors, function(md, idx) {
-        var id;
-        if (md.id === null) {
-          id = path.join(options.unresolvedDir, String(idx));
-        } else {
-          id = md.id;
-        }
-        var filePath = getModulePath(id, options);
-        return writeAstNode(filePath, md.ast);
+      return Bluebird.map(moduleDescriptors, function(md) {
+        return writeAstNode(md.filePath, md.ast);
       });
-      return Bluebird.all(promises);
     });
 }
 
@@ -142,9 +209,20 @@ function decompile(options) {
   return readFile(filePath)
     .then(function (contentStr) {
       var program = esprima.parse(contentStr);
-      var moduleDescriptors = getModuleNodes(program);
-      console.log("Require modules: " + moduleDescriptors.length);
+      program = initTree(program);
+      var defineCalls = markDefineCalls(program);
+      program = markDefineCallsDescendants(program);
+      _.forEach(defineCalls, function(dc) {
+        augmentRootNoConflict(dc);
+      });
+      var moduleDescriptors = getModuleDescriptors(defineCalls, options);
+
       return writeModules(moduleDescriptors, options);
+
+      // console.log("marked");
+      // var moduleDescriptors = getModuleNodes(program);
+      // console.log("Require modules: " + moduleDescriptors.length);
+      // return writeModules(moduleDescriptors, options);
     });
 }
 
